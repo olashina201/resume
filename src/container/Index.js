f5005cef0f776ff68d972bee4c41cb0c57269cbf
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-08-18 02:46:54 +0100

    add toast

--- src/container/Index.js
+++ src/container/Index.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "../assets/style.css";
 import SideBar from "./SideBar";
 import Main from "./Main";
 
 const Index = () => {
+  const notify = () => toast("Sikiru Quadri Olashina");
+
   useEffect(() => {
-    alert("Sikiru Quadri Olashina")
+    notify()
   }, [])
   return (
     <div className="main">
       <SideBar />
       <Main />
+      <ToastContainer />
     </div>
   );
 };
