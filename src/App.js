fa825274c8361df2f53610c3bc08c8e33ca95b70
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-08-18 02:08:53 +0100

    add alert

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Index from "./container/Index";
-import Contact from "./container/Contact";
-import AboutMe from "./container/AboutMe";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/products" component={AboutMe} />
-        </Switch>
-      </Router>
+      <Navbar />
+      <Index />
     </>
   );
 }
--- src/container/Index.js
+++ src/container/Index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../assets/style.css";
 import SideBar from "./SideBar";
 import Main from "./Main";
 
 const Index = () => {
+  useEffect(() => {
+    alert("Sikiru Quadri Olashina")
+  }, [])
   return (
     <div className="main">
       <SideBar />
