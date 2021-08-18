9fd8bd696b42c7186c02a305ee0271b403544da4
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-08-18 01:33:47 +0100

    change link in social component

--- src/container/Social.js
+++ src/container/Social.js
@@ -10,9 +10,9 @@ const Social = () => {
         <div class="boxArea social-menu">
         <ul>
             <li><a href="https://github.com/olashina201" target="blank"><AiOutlineGithub style={{width: "200px"}} /></a></li>
-            <li><a href="https://www.instagram.com/imsanketbodke/" target="blank"><FaInstagram /></a></li>
+            <li><a href="https://www.instagram.com/olashina201" target="blank"><FaInstagram /></a></li>
             <li><a href="https://www.linkedin.com/in/quadri-sikiru-9634a5191/" target="blank"><FaLinkedinIn /></a></li>
-            <li><a href="https://codepen.io/sanketbodke" target="blank"><FaTwitter /></a></li>
+            <li><a href="https://twitter.com/OlashinaQuadri" target="blank"><FaTwitter /></a></li>
         </ul>
     </div>
     )
